b1af214553abe0b8410bf20c071411a326ccee81
EC2 Default User <ec2-user@ip-172-31-46-97.us-east-2.compute.internal>
2019-08-05 03:22:05 +0000

    f

--- twitterAPI.rb
+++ twitterAPI.rb
@@ -51,19 +51,7 @@ end
 
 
 
-def collect_with_max_id(collection=[], max_id=nil, &block)
-  response = yield(max_id)
-  collection += response
-  response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
-end
 
-def client.get_all_tweets(user)
-  collect_with_max_id do |max_id|
-    options = {count: 200, include_rts: true}
-    options[:max_id] = max_id unless max_id.nil?
-    favorites(user, options)
-  end
-end
 
 def getiine
 
@@ -72,6 +60,20 @@ def getiine
   nitizi = kyonen + "のいいね"
   toLINE(nitizi)
   tw = []
+  
+  def collect_with_max_id(collection=[], max_id=nil, &block)
+    response = yield(max_id)
+    collection += response
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
+  
+  def client.get_all_tweets(user)
+    collect_with_max_id do |max_id|
+      options = {count: 200, include_rts: true}
+      options[:max_id] = max_id unless max_id.nil?
+      favorites(user, options)
+    end
+  end
   
   client.get_all_tweets(928272501943046149).each do |tweet|
     
